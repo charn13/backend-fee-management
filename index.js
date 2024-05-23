615854b6a4ec0f73aa0618f83fdc07c2271f080a
Charanjeet singh <152056551+charn13@users.noreply.github.com>
2024-05-23 16:15:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const app = express()
 const port = 5000
 var path = require('path');
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://fee-mangement-front-mka740a8p.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://fee-mangement-front-end.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Delete,Post"
