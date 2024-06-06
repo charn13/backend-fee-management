4e4b64043bc3c2fa6e766939bad89ae0b2cec5df
charanjeet singh <ggumber1998@gmail.com>
2024-06-06 15:55:42 +0530

    add

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 global.foodData = require('./db')(function call(err, data, CatData) {
   // console.log(data)
   if(err) console.log(err);
@@ -12,17 +11,30 @@ const app = express()
 const port = 5000
 var path = require('path');
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://fee-mangement-front-end.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Delete,Post"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Add PUT method
   next();
 });
 // set path for static assets
 app.use(express.static(path.join(__dirname, 'uploads')));
 
+
+
+// const cors = require('cors');
+
+// const corsOptions = {
+//   origin: 'https://localhost:3000', // Your frontend URL
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+//   optionsSuccessStatus: 204
+// };
+
+
+
+// app.use(cors(corsOptions));
 
 app.use(express.json())
 
@@ -36,10 +48,8 @@ app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
 
-
-
-
 // global.foodData = require('./db')(function call(err, data, CatData) {
+//   // console.log(data)
 //   if(err) console.log(err);
 //   global.foodData = data;
 //   global.foodCategory = CatData;
@@ -49,27 +59,20 @@ app.listen(port, () => {
 // const app = express()
 // const port = 5000
 // var path = require('path');
-
-// const allowedOrigins = [
-//   'https://fee-mangement-front-end.vercel.app',
-//   'http://localhost:3000'
-// ];
-
 // app.use((req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 //   res.header(
 //     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Delete, Post"
+//     "Origin, X-Requested-With, Content-Type, Accept"
 //   );
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE"); // Allow all methods
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Add PUT method
 //   next();
 // });
-
+// // set path for static assets
 // app.use(express.static(path.join(__dirname, 'uploads')));
-// app.use(express.json());
+
+
+// app.use(express.json())
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -80,3 +83,48 @@ app.listen(port, () => {
 // app.listen(port, () => {
 //   console.log(`Example app listening on http://localhost:${port}`)
 // })
+
+
+
+
+// // global.foodData = require('./db')(function call(err, data, CatData) {
+// //   if(err) console.log(err);
+// //   global.foodData = data;
+// //   global.foodCategory = CatData;
+// // })
+
+// // const express = require('express')
+// // const app = express()
+// // const port = 5000
+// // var path = require('path');
+
+// // const allowedOrigins = [
+// //   'https://fee-mangement-front-end.vercel.app',
+// //   'http://localhost:3000'
+// // ];
+
+// // app.use((req, res, next) => {
+// //   const origin = req.headers.origin;
+// //   if (allowedOrigins.includes(origin)) {
+// //     res.setHeader("Access-Control-Allow-Origin", origin);
+// //   }
+// //   res.header(
+// //     "Access-Control-Allow-Headers",
+// //     "Origin, X-Requested-With, Content-Type, Accept, Delete, Post"
+// //   );
+// //   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE"); // Allow all methods
+// //   next();
+// // });
+
+// // app.use(express.static(path.join(__dirname, 'uploads')));
+// // app.use(express.json());
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World!')
+// // })
+
+// // app.use('/api/auth', require('./Routes/Auth'));
+
+// // app.listen(port, () => {
+// //   console.log(`Example app listening on http://localhost:${port}`)
+// // })
